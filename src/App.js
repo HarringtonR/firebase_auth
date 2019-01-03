7dae5475ac514f3434197c826a4ea7b3e4f47d03
HarringtonR <ross.ludeke@gmail.com>
2019-01-02 21:28:42 -0600

    firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 export default class App extends Component{
+  componentWillMount(){
+   firebase.initalizeApp({
+
+   })
+  }
+
   render() {
     return (
       <View>
